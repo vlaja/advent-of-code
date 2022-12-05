5d9879501db51a29f49e76bca897697fe6c429f0
Vlatko Vlahek <vlatko@prototyp.digital>
2022-12-05 13:11:52 +0100

    Day 4 part 1

--- src/day-4/day-4.service.ts
+++ src/day-4/day-4.service.ts
@@ -15,11 +15,51 @@ export class Day4Service implements IDailyChallengeService {
   }
 
   private _processData = (response: AxiosResponse<string, string>) => {
-    return response.data.trimEnd().split('\n');
+    return response.data
+      .trimEnd()
+      .split('\n')
+      .map((assignmentPairs: string) =>
+        assignmentPairs
+          .split(',')
+          .map((pair) => pair.split('-').map((val) => +val)),
+      );
+  };
+
+  private _getPositions(elfs: number[][]) {
+    return elfs.map((arr) => {
+      const [min, max] = arr;
+      const keys = max - min;
+      return [...Array(keys + 1).keys()].map((i) => i + min);
+    });
+  }
+
+  private _reducePositions(acc: number[], cur: number[]) {
+    if (!acc.length) {
+      acc = cur;
+      return acc;
+    }
+
+    const fullyEncloses = cur.every((val) => acc.includes(val));
+    if (!fullyEncloses) acc = [];
+    return acc;
+  }
+
+  private _filterFullMatches = (positionSets: number[][]) => {
+    return positionSets
+      .sort((a, b) => b.length - a.length)
+      .reduce(this._reducePositions, []);
   };
 
   solveFirstPart() {
-    return this._processInput().pipe(map((data) => console.log(data)));
+    return this._processInput().pipe(
+      map(
+        (assignmentPair) =>
+          assignmentPair
+            .map(this._getPositions)
+            .map(this._filterFullMatches)
+            .filter((val) => val.length > 0).length,
+      ),
+    );
   }
 
   solveSecondPart() {
