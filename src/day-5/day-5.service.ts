130b5fd5d743cb58cfc9b08819fcca9c8a6dce25
Vlatko Vlahek <vlatko@prototyp.digital>
2022-12-07 18:08:28 +0100

    Finished part one

--- src/day-5/day-5.service.ts
+++ src/day-5/day-5.service.ts
@@ -18,7 +18,7 @@ export class Day5Service implements IDailyChallengeService {
     acc[index] = cur
       .split('/t')
       .reduce(
-        (row, character) => (row = character.match(/.{1,4}/g) || []),
+        (_row, character) => (_row = character.match(/.{1,4}/g) || []),
         new Array<string>(),
       )
       .map((val) => val.trim());
@@ -31,8 +31,12 @@ export class Day5Service implements IDailyChallengeService {
     cur: string[],
   ) => {
     cur.forEach((_, i) => {
-      !acc[i] ? (acc[i] = [cur[i]]) : acc[i].push(cur[i]);
-      acc[i] = acc[i].filter((item) => item && !Number(item));
+      const index = i + 1;
+      !acc[index]
+        ? (acc[index] = [cur[i]])
+        : [cur[i], acc[index].unshift(cur[i])];
+
+      acc[index] = acc[index].filter((item) => item && !Number(item));
     });
 
     return acc;
@@ -66,15 +70,32 @@ export class Day5Service implements IDailyChallengeService {
       );
   };
 
-  solveFirstPart() {
-    return this._processInput().pipe(
-      map(([positions, instructions]) => {
-        console.log(positions);
-        return [];
-      }),
-    );
+  private _moveCrates([positions, instructions]: [
+    Record<number, string[]>,
+    number[][],
+  ]) {
+    for (const instruction of instructions) {
+      const [amount, from, to] = instruction;
+      positions[to] = [
+        ...positions[to],
+        ...positions[from].slice(-amount).reverse(),
+      ];
+
+      positions[from] = positions[from].slice(0, -amount);
+    }
+
+    return Object.values(positions)
+      .map((val) => val.pop() || ' ')
+      .join('')
+      .replace(/[(\[+)(\]+)]/gm, '');
   }
 
+  solveFirstPart() {
+    return this._processInput().pipe(
+      map(this._moveCrates),
+      // map((positions) => console.log(positions)),
+    );
+  }
   solveSecondPart() {
     return this._processInput().pipe(map((assignmentPair) => assignmentPair));
   }
